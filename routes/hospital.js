63071c63a69686907292ee4e45b60fceb922554c
Desarrollador de videojuegos <apinedobarrios@live.com>
2019-01-10 20:58:25 -0600

    Sección 11 terminada - CRUD -Imagenes

--- routes/hospital.js
+++ routes/hospital.js
@@ -1,39 +1,39 @@
 var express = require('express');
 var app = express();
-var bcrypt = require('bcryptjs');
-
-var jwt = require('jsonwebtoken');
 
 var mdAutenthication = require('../middleware/authentication');
-var Usuario = require('../models/usuario');
+var Hospital = require('../models/hospital');
 
 
 // ==========================================
-// Obtener todos los usuarios
+// Obtener todos los Hospitales
 // =========================================
 app.get('/', (req, res, next) => {
 
     var desde = req.query.desde || 0;
     desde = Number(desde);
 
-    Usuario.find({}, 'nombre email img role')
+    Hospital.find({})
         .skip(desde)
         .limit(5)
-        .exec((err, usuarios) => {
+        .populate('usuario', 'nombre email')
+        .exec((err, hospitales) => {
             if (err) {
                 return res.status(500).json({
                     ok: false,
-                    mensaje: 'Error cargando usuarios',
+                    mensaje: 'Error cargando hospitales',
                     errors: err
                 });
 
             }
-            Usuario.count({}, (err, conteo) => {
+            Hospital.count({}, (err, conteo) => {
+
                 res.status(200).json({
                     ok: true,
-                    usuarios: usuarios,
+                    hospitales: hospitales,
                     total: conteo
                 });
+
             });
 
 
@@ -45,49 +45,48 @@ app.get('/', (req, res, next) => {
 
 
 // ==========================================
-// Actualizar usuario
+// Actualizar hospitales
 // =========================================
 
 app.put('/:id', mdAutenthication.verificaToken, (req, res) => {
 
     var id = req.params.id;
     var body = req.body;
-    Usuario.findById(id, (err, usuario) => {
+    Hospital.findById(id, (err, hospital) => {
 
         if (err) {
             return res.status(500).json({
                 ok: false,
-                mensaje: 'Error al buscar usuario',
+                mensaje: 'Error al buscar hospital',
                 errors: err
             });
 
         }
 
-        if (!usuario) {
+        if (!hospital) {
             return res.status(400).json({
                 ok: false,
-                mensaje: 'El usuario con el ID ' + id + ' no existe',
-                errors: { message: 'No existe un usuario con ese ID' }
+                mensaje: 'El hospital con el ID ' + id + ' no existe',
+                errors: { message: 'No existe un hospital con ese ID' }
             });
         }
 
-        usuario.nombre = body.nombre;
-        usuario.email = body.email;
-        usuario.role = body.role;
+        hospital.nombre = body.nombre;
+        hospital.usuario = req.usuario._id;
 
-        usuario.save((err, usuarioGuardado) => {
+        hospital.save((err, hospitalGuardado) => {
             if (err) {
                 return res.status(400).json({
                     ok: false,
-                    mensaje: 'Error al actualizar usuario',
+                    mensaje: 'Error al actualizar hospital',
                     errors: err
                 });
 
             }
-            usuarioGuardado.password = ':D'
+
             res.status(200).json({
                 ok: true,
-                usuario: usuarioGuardado
+                hospital: hospitalGuardado
             });
 
         });
@@ -99,25 +98,22 @@ app.put('/:id', mdAutenthication.verificaToken, (req, res) => {
 
 });
 // ==========================================
-// Crear un nuevo usuario
+// Crear un nuevo hospital
 // =========================================
 app.post('/', mdAutenthication.verificaToken, (req, res) => {
 
     var body = req.body;
-    var usuario = new Usuario({
+    var hospital = new Hospital({
         nombre: body.nombre,
-        email: body.email,
-        password: bcrypt.hashSync(body.password, 10),
-        img: body.img,
-        role: body.role
+        usuario: req.usuario._id
     });
 
-    usuario.save((err, usuarioGuardado) => {
+    hospital.save((err, hospitalGuardado) => {
 
         if (err) {
             return res.status(400).json({
                 ok: false,
-                mensaje: 'Error al crear usuario',
+                mensaje: 'Error al crear hospital',
                 errors: err
             });
 
@@ -125,9 +121,7 @@ app.post('/', mdAutenthication.verificaToken, (req, res) => {
 
         res.status(201).json({
             ok: true,
-            usuario: usuarioGuardado,
-            usuarioToken: req.usuario
-
+            hospital: hospitalGuardado
         });
 
 
@@ -136,33 +130,33 @@ app.post('/', mdAutenthication.verificaToken, (req, res) => {
 });
 
 // ==========================================
-//  Borrar un usuario por el ID
+//  Borrar un hospital por el ID
 // =========================================
 app.delete('/:id', mdAutenthication.verificaToken, (req, res) => {
     var id = req.params.id;
 
-    Usuario.findByIdAndRemove(id, (err, usuarioBorrado) => {
+    Hospital.findByIdAndRemove(id, (err, hospitalBorrado) => {
         if (err) {
             return res.status(500).json({
                 ok: false,
-                mensaje: 'Error al borrar usuario',
+                mensaje: 'Error al borrar hospital',
                 errors: err
             });
 
         }
 
-        if (!usuarioBorrado) {
+        if (!hospitalBorrado) {
             return res.status(400).json({
                 ok: false,
-                mensaje: 'No existe un usuario con ese ID',
-                errors: { mensaje: 'No existe ningun usuario con ese ID' }
+                mensaje: 'No existe un hospital con ese ID',
+                errors: { mensaje: 'No existe ningun hospital con ese ID' }
             });
 
         }
 
         res.status(200).json({
             ok: true,
-            usuario: usuarioBorrado
+            hospital: hospitalBorrado
         });
     });
 });
